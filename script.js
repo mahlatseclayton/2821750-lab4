bbb720108f71bee09f044f120400f656bad292af
mahlatseclayton <mahlatseclayton1@gmail.com>
2026-03-02 14:51:08 +0200

    updated styles.css

---
+++ script.js
@@ -0,0 +1,11 @@
+const searchBtn=document.getElementById("search-btn");
+if(searchBtn){
+   const countryName=document.getElementById("country-input").value;
+   if(countryName!=""){
+        // search country
+        
+   }
+   else{
+    // show error 
+   }
+}
